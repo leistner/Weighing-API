49796de2ea4e075bddb10d40734a448fa62a5f9d
leistner <dirk.leistner@hbm.com>
2019-05-07 12:34:53 +0200

    Nw command approach

---
+++ HBM.Weighing.API/WTX/Jet/JetBusCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HBM.Weighing.API.WTX.Jet
+{
+    public class JetBusCommand
+    {
+        public JetBusCommand(int DataType, string Path, int BitIndex, int BitLength);
+
+        int DataType { get; }
+
+        string Path { get; }
+
+        int BitIndex { get; }
+
+        int BitLength { get; }
+
+    }
+}
